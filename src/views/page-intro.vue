18d69463bdf3ecb5311f08f4cd33c04ba277bf7d
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-16 19:52:40 +0800

    update the navbar

---
+++ src/views/page-intro.vue
@@ -0,0 +1,26 @@
+<script>
+/**
+ * Page-intro component
+ */
+import Navbar from "@/components/navbar"
+import Footer from "@/components/footer"
+
+export default {
+    data() {
+        return {}
+    },
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
+
+<template>
+    <div>
+        <Navbar :nav-light="true" />
+        <Footer />
+
+    </div>
+</template>
+<style scoped></style>
